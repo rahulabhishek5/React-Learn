cdd6598c1a4c666484b5acd08034e47b318f98bb
K Rahul Abhishek <rahul0987abhishek@gmail.com>
2026-06-07 18:28:04 +0530

    feat: add App component demonstrating useEffect dependency management with state updates

--- 16-useEffectHook/src/App.jsx
+++ 16-useEffectHook/src/App.jsx
@@ -70,29 +70,3 @@ const App = () => {
 }
 
 export default App
-
-
-
-
-
-
-
-
-// import React from 'react'
-
-// const App = () => {
-
-//   const getData = () => {
-//     console.log("Button Clicked");
-//   }
-
-//   return (
-//     <div className='bg-black h-screen p-4 text-white'>
-//       <button 
-//         onClick={getData}
-//         className='bg-green-600 text-white px-4 py-2 mb-3 rounded cursor-pointer active:scale-95 transition-all duration-200'>Get Data</button>
-//     </div>
-//   )
-// }
-
-// export default App
